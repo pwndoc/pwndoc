cfb9d2c6cec035c10ae9df04d6a1c97492b4688e
yeln4ts <emile.hammer@gmail.com>
2020-06-12 13:58:09 -0400

    Fix vulnerabilities export in Firefox

--- frontend/src/pages/datas/dump/dump.js
+++ frontend/src/pages/datas/dump/dump.js
@@ -205,8 +205,10 @@ export default {
             var a = document.createElement('a');
             a.href = url;
             a.download = "vulnerabilities.yml";
+            document.body.appendChild(a);
             a.click();
             URL.revokeObjectURL(url);
+            document.body.removeChild(a);
             
         },
 
