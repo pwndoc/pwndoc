011d9d2b5c05cc5925af9478da0e0bee77f8aa93
yeln4ts <emile.hammer@gmail.com>
2021-06-02 16:40:05 -0400

    Add Trim to all strings saved in database
    Avoid issues like additionnal spaces in titles

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,8 @@ global.__basedir = __dirname;
 var mongoose = require('mongoose');
 // Use native promises
 mongoose.Promise = global.Promise;
+// Trim all Strings
+mongoose.Schema.Types.String.set('trim', true);
 
 mongoose.connect(`mongodb://${config.database.server}:${config.database.port}/${config.database.name}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
 
