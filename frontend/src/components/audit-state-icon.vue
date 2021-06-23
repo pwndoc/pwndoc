d1cc9d913eedb582e61740e10eb0048819a2fc06
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-06-22 21:01:40 -0400

    Fixed audit-state-icon to wait for settings.

--- frontend/src/components/audit-state-icon.vue
+++ frontend/src/components/audit-state-icon.vue
@@ -2,7 +2,7 @@
     <q-icon v-if="!state" :size="size" style="visibility: hidden" flat color="warning" name="fas fa-question"></q-icon>
     <q-icon v-else-if="state === 'APPROVED'" :size="size" flat color="positive" name="far fa-check-circle">
         <q-tooltip anchor="bottom middle" self="center left" :delay="500" content-class="text-bold">
-            Audit is approved ({{ getApprovalCount() }}/{{ $settings.reviews.settings.minReviewers }})
+            Audit is approved ({{ getApprovalCount() }}/{{ getMinReviewers() }})
             <div v-for="reviewer in approvals">
                 <q-separator />
                 {{ reviewer.firstname }} {{ reviewer.lastname }} 
@@ -11,7 +11,7 @@
     </q-icon>
     <q-icon v-else-if="state === 'REVIEW'" :size="size" flat color="warning" name="fas fa-exclamation-triangle">
         <q-tooltip anchor="bottom middle" self="center left" :delay="500" content-class="text-bold">
-            Audit is being reviewed ({{ getApprovalCount() }}/{{ $settings.reviews.settings.minReviewers }})
+            Audit is being reviewed ({{ getApprovalCount() }}/{{ getMinReviewers() }})
             <div v-for="reviewer in approvals">
                 <q-separator />
                 {{ reviewer.firstname }} {{ reviewer.lastname }}
@@ -33,8 +33,13 @@ export default {
 
     methods: {
         getApprovalCount() {
-            if(!this.approvals) return -1;
-            else return this.approvals.length;
+            if(this.approvals) return this.approvals.length;
+            else return -1;
+        },
+
+        getMinReviewers() {
+            if(this.$settings) return this.$settings.reviews.settings.minReviewers;
+            else return -1;
         }
     }
 }
