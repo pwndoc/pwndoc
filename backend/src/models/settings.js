7922af2e0c069d45073c323ffa802ec1e5d8fef2
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-06-22 20:20:35 -0400

    Refactored settings backend.
    Moved all settings logic to model.
    
    Added inheritance for setting sections.
    
    Changed model structure to allow more scalability.
    
    This commit BREAKS frontend. Needs additional commit for complete release.

--- backend/src/models/settings.js
+++ backend/src/models/settings.js
@@ -1,41 +1,146 @@
 var mongoose = require('mongoose');//.set('debug', true);
 var Schema = mongoose.Schema;
+var fs = require('fs');
 
-var SettingSchema = new Schema({
-    imageBorder: { type: Boolean, default: false },
-    imageBorderColor: { type: String, default: "#000000" },
-    cvssColors : {
-        noneColor : { type: String, default: "#4a86e8" },
-        lowColor : { type: String, default: "#008000" },
-        mediumColor : { type: String, default: "#f9a009" },
-        highColor : { type: String, default: "#fe0000" },
-        criticalColor : { type: String, default: "#212121" } 
-    },
-    enableReviews: { type: Boolean, default: false },
-    mandatoryReview: { type: Boolean, default: false },
-    minReviewers: { type: Number, default: 1 },
-    removeApprovalsUponUpdate: { type: Boolean, default: false }
-}, {
-    collection: 'settings',
-    capped: { max: 1 }
+const BaseSettingSchema = {
+    enabled: { type: Boolean, default: true },
+    public: { type: Map, of: Schema.Types.Mixed, default: {} },
+    private: { type: Map, of: Schema.Types.Mixed, default: {} },
+};
+
+// https://stackoverflow.com/questions/25822289/what-is-the-best-way-to-store-color-hex-values-in-mongodb-mongoose
+const colorValidator = (v) => (/^#([0-9a-f]{3}){1,2}$/i).test(v);
+
+const ReportSettingSchema = new Schema({
+    ...BaseSettingSchema,
+    private: {
+        imageBorder: { type: Boolean, default: false },
+        imageBorderColor: { type: String, default: "#000000", validate: [colorValidator, 'Invalid color'] },
+        cvssColors: {
+            noneColor: { type: String, default: "#4a86e8", validate: [colorValidator, 'Invalid color'] },
+            lowColor: { type: String, default: "#008000", validate: [colorValidator, 'Invalid color'] },
+            mediumColor: { type: String, default: "#f9a009", validate: [colorValidator, 'Invalid color'] },
+            highColor: { type: String, default: "#fe0000", validate: [colorValidator, 'Invalid color'] },
+            criticalColor: { type: String, default: "#212121", validate: [colorValidator, 'Invalid color'] }
+        },
+    }
 });
 
-// Get settings
-SettingSchema.statics.getSettings = () => {
-    return new Promise((resolve, reject) => {
-        var query = Settings.findOne();
-        query.select('-_id -__v');
-        query.exec()
-        .then((out) => {
-            resolve(out);
-        })
-        .catch((err) => {
-            reject(err);
-        })
-    });
+const ReviewSettingSchema = new Schema({
+    ...BaseSettingSchema,
+    enabled: { type: Boolean, default: false },
+    public: {
+        mandatoryReview: { type: Boolean, default: false },
+        minReviewers: { type: Number, default: 1, min: 1, max: 100, validate: [Number.isInteger, 'Invalid integer'] }
+    },
+    private: {
+        removeApprovalsUponUpdate: { type: Boolean, default: false }
+    }
+});
+
+const SettingSchema = new Schema({
+    report: { type: ReportSettingSchema },
+    reviews: { type: ReviewSettingSchema }
+});
+
+const APP_SETTINGS_FILE = `${__basedir}/app-settings.json`;
+const BASE_SELECT_QUERY = "-_id -__v -report._id -reviews._id ";
+
+// Merges settings public and private field.
+function mergeSettings(settings) {
+    if (settings._doc) settings = settings.toObject();
+
+    return Object.entries(settings).reduce((obj, [name, setting]) => {
+        if (setting) return {
+            ...obj,
+            [name]: {
+                ...setting,
+                settings: { ...(setting.public || {}), ...(setting.private || {}) },
+                public: undefined,
+                private: undefined
+            }
+        };
+        else return obj;
+    }, {});
 }
 
+// Unmerges single settings field into public and private (if not set).
+function unmergeSettings(settings) {
+    if (!settings) return settings;
 
+    return Object.entries(settings).reduce((obj, [name, setting]) => {
+        if (!setting.public) setting = { ...setting, public: setting.settings };
+        if (!setting.private) setting = { ...setting, private: setting.settings };
+        return { ...obj, [name]: setting };
+    }, {});
+}
 
-var Settings = mongoose.model('Settings', SettingSchema);
+// Removes settings that not set to enabled.
+function removeDisabledSetting(settings) {
+    if (settings._doc) settings = settings.toObject();
+
+    return Object.entries(settings).reduce((obj, [name, setting]) => {
+        if (setting.enabled) return {
+            ...obj, [name]: { ...setting, enabled: undefined }
+        };
+        else return obj;
+    }, {});
+}
+
+// Get settings
+SettingSchema.statics.getAll = () => {
+    return new Promise((resolve, reject) => {
+        const query = Settings.findOne({});
+        query.select(BASE_SELECT_QUERY);
+        query.exec()
+            .then(settings => resolve(mergeSettings(settings)))
+            .catch(err => reject(err));
+    });
+};
+
+// Get public settings
+SettingSchema.statics.getPublic = () => {
+    return new Promise((resolve, reject) => {
+        const query = Settings.findOne({});
+        query.select(BASE_SELECT_QUERY + "-report.private -reviews.private ");
+        query.exec()
+            .then(settings => resolve(mergeSettings(removeDisabledSetting(settings))))
+            .catch(err => reject(err));
+    });
+};
+
+// Update settings
+SettingSchema.statics.update = (settings) => {
+    settings = unmergeSettings(settings);
+
+    return new Promise((resolve, reject) => {
+        const query = Settings.findOneAndUpdate({}, settings, { new: true });
+        query.select(BASE_SELECT_QUERY);
+        query.exec()
+            .then(settings => resolve(mergeSettings(settings)))
+            .catch(err => reject(err));
+    });
+};
+
+// Restore settings to default
+SettingSchema.statics.restoreDefaults = () => {
+    return new Promise(async (resolve, reject) => {
+        let defaultSettings = {};
+        if (fs.existsSync(APP_SETTINGS_FILE)) {
+            defaultSettings = JSON.parse(fs.readFileSync(APP_SETTINGS_FILE));
+        }
+
+        const query = Settings.deleteMany({});
+        query.exec()
+            .then(_ => {
+                const query = new Settings(defaultSettings);
+                query.save()
+                    .then(_ => resolve("Restored default settings."))
+                    .catch(err => reject(err));
+            })
+            .catch(err => reject(err));
+    });
+};
+
+const Settings = mongoose.model('Settings', SettingSchema);
 module.exports = Settings;
