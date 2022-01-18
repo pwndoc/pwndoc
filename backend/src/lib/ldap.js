8956e36a92e72d45c066f6ba9ef9de47f960a1f3
Paul <git@zhio.de>
2022-01-18 12:14:24 +0100

    Made a small ldap lib

---
+++ backend/src/lib/ldap.js
@@ -0,0 +1,72 @@
+const env = process.env.NODE_ENV || 'dev'
+const config = require('../config/config.json')
+const ldap = require("ldapjs");
+
+/**
+ * This searches LDAP for an entry based on username
+ * @param {ldapjs.Client} client The client to use
+ * @param {string} username The username to search
+ * @returns {Promise<object>} Promise resolves with the user object
+ */
+function search(client, username) {
+  return new Promise((resolve, reject) => {
+    client.search(
+      config[env].ldap.userDN.replace("%u", username),
+      {},
+      (err, resp) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resp.on("searchEntry", function (entry) {
+          resolve(entry.object);
+        });
+        resp.on("error", function (err) {
+          reject(err);
+        });
+      }
+    );
+  });
+}
+
+/**
+ * This binds the client.
+ * @param {ldapjs.Client} client The client to use
+ * @param {string} username The username to bind
+ * @param {string} password The password to use on binding process
+ * @returns {Promise} Promise resolves when binding was good.
+ */
+function bind(client, username, password) {
+  return new Promise((resolve, reject) => {
+    client.bind(
+      config[env].ldap.userDN.replace("%u", username),
+      password,
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
+}
+
+module.exports = (username, password) =>
+  new Promise((resolve, reject) => {
+    let client = ldap.createClient({
+      url: config[env].ldap.address,
+    });
+    client.on("error", function (err) {
+      reject(err);
+    });
+    bind(client, username, password)
+      .then(() => search(client, username))
+      .then((resp) => {
+        client.unbind();
+        resolve(resp);
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
