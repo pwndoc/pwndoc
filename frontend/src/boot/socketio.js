2673749cd02b8bc0827af35d97d14b7e93c33783
yeln4ts <emile.hammer@gmail.com>
2022-02-23 15:34:16 -0500

    Add dynamic check for backend connection
    If websocket disconnect then a loading message appear until it reconnects

--- frontend/src/boot/socketio.js
+++ frontend/src/boot/socketio.js
@@ -1,5 +1,22 @@
 import io from 'socket.io-client'
+import {Loading} from 'quasar'
+import { $t } from '@/boot/i18n'
 
 export default ({ Vue }) => {
-    Vue.prototype.$socket = io(`${window.location.protocol}//${window.location.hostname}:${process.env.API_PORT}`);
+    var socket = io(`${window.location.protocol}//${window.location.hostname}:${process.env.API_PORT}`);
+
+    socket.on('disconnect', (error) => {
+        Loading.show({
+            message: `<i class='material-icons'>wifi_off</i><br /><p>${$t('msg.wrongContactingBackend')}</p>`, 
+            spinner: null, 
+            backgroundColor: 'red-10', 
+            customClass: 'loading-error',
+            delay: 5000
+        })
+    })
+    socket.on('connect', () => {    
+        Loading.hide()
+    })
+
+    Vue.prototype.$socket = socket
 }  
