ac686d2add3bb594a1673bfce5c1ba579844eb2f
WhiteWinterWolf <github@whitewinterwolf.com>
2022-03-13 18:22:11 +0100

    Add new filters code

--- backend/src/lib/report-generator.js
+++ backend/src/lib/report-generator.js
@@ -109,6 +109,31 @@ exports.generateDoc = generateDoc;
 
 // *** Angular parser filters ***
 
+// Creates text block bookmark: {name | bookmarkCreate: identifier}
+// Bookmark identifiers need to begin with a letter and contain only letters, numbers, and underscore characters, dashes are automatically replaced by underscores.
+expressions.filters.bookmarkCreate = function(input, refid) {
+    let rand_id = Math.floor(Math.random() * 1000000 + 1000);
+
+    return '<w:bookmarkStart w:id="' + rand_id + '" '
+        + 'w:name="' + refid.replace('-', '_') + '"/>'
+        + input
+        + '<w:bookmarkEnd w:id="' + rand_id + '"/>';
+}
+
+// Creates a clickable reference to an previously created bookmark: {identifier | bookmarkLink}
+expressions.filters.bookmarkRef = function(input) {
+    return '<w:r><w:fldChar w:fldCharType="begin"/></w:r><w:r><w:instrText xml:space="preserve">'
+        + ' REF ' + input.replaceAll('-', '_') + ' \\h </w:instrText></w:r>'
+        + '<w:r><w:fldChar w:fldCharType="separate"/></w:r><w:r><w:t>'
+        + input + '</w:t></w:r><w:r><w:fldChar w:fldCharType="end"/></w:r>';
+}
+
+// Capitalizes input first letter: {input | capfirst}
+expressions.filters.capfirst = function(input) {
+    if (!input || input == "undefined") return input;
+    return input.replace(/^\w/, (c) => c.toUpperCase());
+}
+
 // Convert input date with parameter s (full,short): {input | convertDate: 's'}
 expressions.filters.convertDate = function(input, s) {
     var date = new Date(input);
@@ -147,6 +172,114 @@ expressions.filters.changeID = function (input, prefix) {
     return input.replace("IDX-", prefix);
 }
 
+// Default value: returns input if it is truthy, otherwise its parameter.
+// Example producing a comma-separated list of affected systems, falling-back on the whole audit scope: {affected | lines | d: (scope | select: 'name') | join: ', '}
+expressions.filters.d = function(input, s) {
+    return (input && input != "undefined") ? input : s;
+}
+
+// Display "From ... to ..." dates nicely, removing redundant information when the start and end date occur during the same month or year: {date_start | fromTo: date_end:'fr' | capfirst}
+// To internationalize or customize the resulting string, associate the desired output to the strings "from {0} to {1}" and "on {0}" in your Pwndoc translate file.
+expressions.filters.fromTo = function(start, end, locale) {
+    const start_date = new Date(start);
+    const end_date = new Date(end);
+    let options = {}, start_str = '', end_str = '';
+    let str = "from {0} to {1}";
+
+    if (start_date == "Invalid Date" || end_date == "Invalid Date") return start;
+
+    options = {day: '2-digit', month: '2-digit', year: 'numeric'};
+    end_str = end_date.toLocaleDateString(locale, options);
+
+    if (start_date.getYear() != end_date.getYear()) {
+        options = {day: '2-digit', month: '2-digit', year: 'numeric'};
+        start_str = start_date.toLocaleDateString(locale, options);
+    }
+    else if (start_date.getMonth() != end_date.getMonth()) {
+        options = {day: '2-digit', month: '2-digit'};
+        start_str = start_date.toLocaleDateString(locale, options);
+    }
+    else if (start_date.getDay() != end_date.getDay()) {
+        options = {day: '2-digit'};
+        start_str = start_date.toLocaleDateString(locale, options);
+    }
+    else {
+        start_str = end_str;
+        str = "on {0}";
+    }
+
+    return translate.translate(str).format(start_str, end_str);
+}
+
+// Group input elements by an attribute: {#findings | groupBy: 'severity'}{title}{/findings | groupBy: 'severity'}
+// Source: https://stackoverflow.com/a/34890276
+expressions.filters.groupBy = function(input, key) {
+    return input.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+}
+
+// Returns the initials from an input string (typically a firstname): {creator.firstname | initials}
+expressions.filters.initials = function(input) {
+    if (!input || input == "undefined") return input;
+    return input.replace(/(\w)\w+/gi,"$1.");
+}
+
+// Returns a string which is a concatenation of input elements using an optional separator string: {scope | join: ', '}
+// Can also be used to build raw OOXML strings.
+expressions.filters.join = function(input, sep = '') {
+    if (!input || input == "undefined") return input;
+    return input.join(sep);
+}
+
+// Returns the length (ie. number of items for an array) of input: {input | length}
+// Can be used as a conditional to check the emptiness of a list: {#input | length}Not empty{/input | length}
+expressions.filters.length = function(input) {
+    return input.length;
+}
+
+// Takes a multilines input strings (either raw or simple HTML paragraphs) and returns each line as an ordered list: {input | lines}
+expressions.filters.lines = function(input) {
+    if (!input || input == "undefined") return input;
+    if (input.indexOf('<p>') == 0) {
+        return input.substring(3,input.length - 4).split('</p><p>');
+    }
+    else {
+        return input.split("\n");
+    }
+}
+
+// Loop over the input object, providing acccess to its keys and values: {#findings | loopObject}{key}{value.name}{/findings | loopObject}
+// Source: https://stackoverflow.com/a/60887987
+expressions.filters.loopObject = function(input) {
+    return Object.keys(input).map(function(key) {
+        return { key , value : input[key]};
+    });
+}
+
+// Lowercases input: {input | lower}
+expressions.filters.lower = function(input) {
+    if (!input || input == "undefined") return input;
+        return input.toLowerCase();
+}
+
+// Creates a clickable "mailto:" link, assumes that input is an email address if no other address has been provided as parameter: {lastname | mailto: email}
+expressions.filters.mailto = function(input, address = null) {
+    return '<w:r><w:fldChar w:fldCharType="begin"/></w:r>'
+        + '<w:r><w:instrText xml:space="preserve"> HYPERLINK "mailto:' + (address ? adddress : input) + '" </w:instrText></w:r>'
+        + '<w:r><w:fldChar w:fldCharType="separate"/></w:r>'
+        + '<w:r><w:rPr><w:rStyle w:val="Hyperlink"/></w:rPr>'
+        + '<w:t>' + input + '</w:t>'
+        + '</w:r><w:r><w:fldChar w:fldCharType="end"/></w:r>';
+}
+
+// Applies a filter on a sequence of objects: {scope | select: 'name' | map: lower | join: ', '}
+expressions.filters.map = function(input, filter) {
+    let args = Array.prototype.slice.call(arguments, 2);
+    return input.map(x => expressions.filters[filter](x, ...args));
+}
+
 // Replace newlines in office XML format: {@input | NewLines}
 expressions.filters.NewLines = function(input) {
     var pre = '<w:p><w:r><w:t>';
@@ -166,6 +299,41 @@ expressions.filters.NewLines = function(input) {
     return result;
 }
 
+// Embeds input within OOXML paragraph tags, applying an optional style name to it: {@input | p: 'Some style'}
+expressions.filters.p = function(input, style = null) {
+    let result = '<w:p>';
+
+    if (style !== null ) {
+        let style_parsed = style.replaceAll(' ', '');
+        result += '<w:pPr><w:pStyle w:val="' + style_parsed + '"/></w:pPr>';
+    }
+    result += input + '</w:p>';
+
+    return result;
+}
+
+// Reverses the input array: {input | reverse}
+expressions.filters.reverse = function(input) {
+    return input.reverse();
+}
+
+// Looks up an attribute from a sequence of objects, doted notation is supported: {findings | select: 'cvss.environmentalSeverity'}
+expressions.filters.select = function(input, attr) {
+    return input.map(function(item) { return _getPropertyValue(item, attr) });
+}
+
+// Sorts the input array according an optional given attribute, dotted notation is supported: {#findings | sort 'cvss.environmentalSeverity'}{name}{/findings | sort 'cvss.environmentalSeverity'}
+expressions.filters.sort = function(input, key = null) {
+    if (key === null) {
+        return input.sort();
+    }
+    else {
+        return input.sort(function(a, b) {
+            return _getPropertyValue(a, key) < _getPropertyValue(b, key);
+        });
+    }
+}
+
 // Sort array by supplied field: {#findings | sortArrayByField: 'identifier':1}{/}
 // order: 1 = ascending, -1 = descending
 expressions.filters.sortArrayByField = function (input, field, order) {
@@ -178,6 +346,37 @@ expressions.filters.sortArrayByField = function (input, field, order) {
     })    
     return sorted;
 }
+
+// Takes a string as input and split it into an ordered list using a separator: {input | split: ', '}
+expressions.filters.split = function(input, sep) {
+    if (!input || input == "undefined") return input;
+    return input.split(sep);
+}
+
+// Capitalizes input first letter of each word, can be associated to 'lower' to normalize case: {creator.lastname | lower | title}
+expressions.filters.title= function(input) {
+    if (!input || input == "undefined") return input;
+    return input.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
+}
+
+// Returns the JSON representation of the input value, useful to dump variables content while debugging a template: {input | toJSON}
+expressions.filters.toJSON = function(input) {
+    return JSON.stringify(input);
+}
+
+// Upercases input: {input | upper}
+expressions.filters.upper = function(input) {
+    if (!input || input == "undefined") return input;
+    return input.toUpperCase();
+}
+
+// Filters input elements matching a free-form Angular statements: {#findings | where: 'cvss.severity == "Critical"'}{title}{/findings | where: 'cvss.severity == "Critical"'}
+// Source: https://docxtemplater.com/docs/angular-parse/#data-filtering
+expressions.filters.where = function(input, query) {
+    return input.filter(function (item) {
+        return expressions.compile(query)(item);
+    });
+};
 
 // Convert HTML data to Open Office XML format: {@input | convertHTML: 'customStyle'}
 expressions.filters.convertHTML = function(input, style) {
@@ -210,6 +409,25 @@ expressions.filters.translate = function(input, locale) {
     if (!input) return input
     return $t(input, locale)
 }
+
+// Filters helper: handles the use of dotted notation as property names.
+// Source: https://stackoverflow.com/a/37510735
+function _getPropertyValue(obj, dataToRetrieve) {
+  return dataToRetrieve
+    .split('.')
+    .reduce(function(o, k) {
+      return o && o[k];
+    }, obj);
+}
+
+// Filters helper: handles the use of preformated easilly translatable strings.
+// Source: https://www.tutorialstonight.com/javascript-string-format.php
+String.prototype.format = function () {
+    let args = arguments;
+    return this.replace(/{([0-9]+)}/g, function (match, index) {
+        return typeof args[index] == 'undefined' ? match : args[index];
+    });
+};
 
 // Compile all angular expressions
 var angularParser = function(tag) {
