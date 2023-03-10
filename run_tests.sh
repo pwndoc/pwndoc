fe0964d7f5d634131496f91d71d16abc687192b2
yeln4ts <emile.hammer@gmail.com>
2023-03-10 14:39:07 -0500

    Fix return code for tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 function usage {
-    echo "Usage:        $0 -q|-f [-h, --help]"
+    echo "Usage:        $0 -f [-h, --help]"
     echo ""
     echo "Options:"
     echo "  -h, --help  Display help"
-    echo "  -q          Run quick tests (No build)"
     echo "  -f          Run full tests (Build with no cache)"
     exit 1
 }
 
 function full_test {
-    docker-compose down
-    rm -rf backend/mongo-data-test
+    docker-compose stop
     docker-compose -f backend/docker-compose.test.yml build
     docker-compose -f backend/docker-compose.test.yml run --rm backend-test
-    docker-compose -f backend/docker-compose.test.yml down
-    docker-compose -f backend/docker-compose.test.yml rm
     rc=$?
-    rm -rf backend/mongo-data-test
+    docker-compose -f backend/docker-compose.test.yml down
     exit $rc
 }
 
