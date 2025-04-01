11ec03a45dbe5956504f21a94a3f25b21df4e466
yeln4ts <emile.hammer@gmail.com>
2025-04-01 16:22:04 -0400

    Fix image permissions
    Added an index to handle uniqueness by value and auditId
    Allow to access images without auditId

--- backend/src/models/image.js
+++ backend/src/models/image.js
@@ -1,15 +1,19 @@
 var mongoose = require('mongoose')
+var Audit = require('mongoose').model('Audit');
+
 var Schema = mongoose.Schema
 
 var ImageSchema = new Schema({
     auditId:    {type: Schema.Types.ObjectId, ref: 'Audit'},
-    value:      {type: String, required: true, unique: true},
+    value:      {type: String, required: true},
     name:       String
 
 }, {timestamps: true})
 
-var Audit = require('mongoose').model('Audit');
-
+ImageSchema.index({"auditId": 1, "value": 1}, {
+    name: "unique_audit_value",
+    unique: true
+})
 /*
 *** Statics ***
 */
@@ -18,64 +22,57 @@ var Audit = require('mongoose').model('Audit');
 ImageSchema.statics.getOne = (isAdmin, userId, imageId) => {
     return new Promise((resolve, reject) => {
         var query = Image.findById(imageId);
-        
-        // Check if we have access to the report in which the image is located
-        if (!isAdmin) {
-            query.populate({
-                path: 'auditId',
-                select: '_id',
-                match: { $or: [ {creator: userId}, {collaborators: userId}, {reviewers: userId} ] }
-            });
-        }
-        else {
-            query.populate({
-                path: 'auditId',
-                select: '_id'
-            });
-        }
-
+        query.populate('auditId', '_id creator collaborators reviewers')
         query.select('auditId name value');
-            query.exec()
-            .then((row) => {
-                if (row && row.auditId && row.auditId._id) {
-                    row.auditId = row.auditId._id;
-                    resolve(row);
-                }
-                else {
-                    throw({fn: 'NotFound', message: 'Image not found'});
-                }
-            })
-            .catch((err) => {
-                reject(err)
-            });
+        query.exec()
+        .then((row) => {
+            if (!row)
+                throw({fn: 'NotFound', message: 'Image not found'});
+            else if (
+                isAdmin || row.auditId === null ||
+                row.auditId.creator === userId ||
+                row.auditId.collaborators.includes(userId) ||
+                row.auditId.reviewers.includes(userId)
+            ) {
+                if (row.auditId) row.auditId = row.auditId._id;
+                resolve(row);
+            }
+            else {
+                throw({fn: 'Forbidden', message: 'Insufficient privileges'});
+            }
+        })
+        .catch((err) => {
+            reject(err)
+        });
     })
 }
 
 // Create image
 ImageSchema.statics.create = (isAdmin, userId, image) => {
-    return new Promise((resolve, reject) => {
-        Audit.getAudit(isAdmin, image.auditId, userId)
-            .then((_) => {
-                var query = Image.findOne({value: image.value});
-                query.exec()
-                    .then(row => {
-                        if (row)
-                            return row
-                        query = new Image(image)
-                        return query.save()
-                    })
-                    .then((row) => {
-                        resolve({_id: row._id})
-                    })
-                    .catch((err) => {
-                        console.log(err)    
-                        reject(err)
-                    });
-            })
-            .catch((err) => {
-                console.log(err)    
-                reject(err)
-            });
+    return new Promise(async (resolve, reject) => {
+        let audit = null
+        if (image.auditId)
+           audit = await Audit.getAudit(isAdmin, image.auditId, userId)
+        if (!image.auditId || audit) {
+            let query = Image.findOne({value: image.value, auditId: image.auditId});
+            query.exec()
+                .then(row => {
+                    console.log(row)
+                    if (row)
+                        return row
+                    query = new Image(image)
+                    return query.save()
+                })
+                .then((row) => {
+                    resolve({_id: row._id})
+                })
+                .catch((err) => {
+                    console.log(err)    
+                    reject(err)
+                });
+            }
+        else
+            throw({fn: 'Forbidden', message: 'Insufficient privileges'})
     })
 }
 
